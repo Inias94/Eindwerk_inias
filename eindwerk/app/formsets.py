8f67a18c6981d10093f280a0dffc36aba90066fe
Inias94 <iniasderoeck@hotmail.com>
2024-06-15 18:06:28 +0200

    formset toegevoegd om meerdere gerechten aan een menu te kunnen toevoegen.

--- eindwerk/app/formsets.py
+++ eindwerk/app/formsets.py
@@ -1,11 +1,14 @@
 # Django imports
-from django.forms import inlineformset_factory
+from django.forms import inlineformset_factory, modelformset_factory
 
 # Project imports
-from .models import Dish, ProductDish
-from .forms import ProductDishForm
+from .models import Dish, ProductDish, DishMenu
+from .forms import ProductDishForm, DishMenuForm
 
 # Formset for creating multiple products while creating a Dish.
 ProductDishFormSet = inlineformset_factory(
     Dish, ProductDish, form=ProductDishForm, can_delete=True, extra=0
 )
+
+# Formset for creating a menu with multiple dishes.
+DishMenuFormSet = modelformset_factory(DishMenu, form=DishMenuForm, extra=0, can_delete=True)
