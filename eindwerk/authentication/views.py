4555c61257ce5df2f521e0f59670a0ff8a9b515b
Inias94 <iniasderoeck@hotmail.com>
2024-06-15 18:11:45 +0200

    fix om de settings.py module te gebruiken. reeds gedaan in andere files.

--- eindwerk/authentication/views.py
+++ eindwerk/authentication/views.py
@@ -1,8 +1,7 @@
 from django.contrib.auth import logout as django_logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
-
-from eindwerk.settings import SOCIAL_AUTH_AUTH0_DOMAIN, SOCIAL_AUTH_AUTH0_KEY
+from django.conf import settings
 
 
 # Auth0 view. This is the only one required. The rest is handled in the settings file of shopMyDish.
@@ -15,8 +14,8 @@ def logout(request):
     # Log out the user using Django's logout function
     django_logout(request)
     # Get the Auth0 domain and client ID from the settings
-    domain = SOCIAL_AUTH_AUTH0_DOMAIN
-    client_id = SOCIAL_AUTH_AUTH0_KEY
+    domain = settings.SOCIAL_AUTH_AUTH0_DOMAIN
+    client_id = settings.SOCIAL_AUTH_AUTH0_KEY
     # Set the return URL to the current domain
     return_to = "http://127.0.0.1:8000"  # this can be current domain
     # Redirect the user to the Auth0 logout page with the client ID and return URL
