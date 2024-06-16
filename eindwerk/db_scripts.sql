3e028421b38f937578b2f7ec4d774d6e923bf718
Inias94 <iniasderoeck@hotmail.com>
2024-06-16 22:02:32 +0200

    sql script voor units te vullen in db

---
+++ eindwerk/db_scripts.sql
@@ -0,0 +1,12 @@
+INSERT INTO unit (name, abbreviation) VALUES
+  ('Gram', 'g'),
+  ('Milliliter', 'ml'),
+  ('Liter', 'l'),
+  ('Kilogram', 'kg'),  -- Added kilogram
+  ('Centiliter', 'cl'),  -- Added centiliter
+  ('Eetlepel', 'EL'),
+  ('Theelepel', 'TL'),
+  ('Mespunt', 'mpt'),
+  ('Kop', 'kop'),
+  ('Stuk', 'st'),
+  ('Teentje', 'tn');  -- Added teentje
