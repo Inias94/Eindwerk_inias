886387335ee0e61f891f7c910439f74a337b44d8
Inias94 <iniasderoeck@hotmail.com>
2024-06-18 09:55:49 +0200

    dcostring aan de models nog toegevoegd

--- eindwerk/app/models.py
+++ eindwerk/app/models.py
@@ -1,11 +1,10 @@
 from django.utils import timezone
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
-    """The user model inherits data from the auth0 user. Only name & email is used"""
+    """The user model inherits data from the auth0 user. Only name & email is used."""
 
     def __str__(self) -> str:
         return f"{self.username}"
@@ -13,12 +12,11 @@ class User(AbstractUser):
 
 class UserProduct(models.Model):
     """This model represents the relation of a user and a product.
-    It's done this way so people could see what products other users use(future proof).
+    It's done this way so people could see what products other users use (future proof).
     """
 
     # Foreign keys
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # product = models.ForeignKey("Product", on_delete=models.CASCADE, unique=True)
     product = models.OneToOneField("Product", on_delete=models.CASCADE)
 
     def __str__(self) -> str:
@@ -30,7 +28,6 @@ class UserDish(models.Model):
 
     # Foreign keys
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_dishes")
-    # dish = models.ForeignKey("Dish", on_delete=models.CASCADE, unique=True)
     dish = models.OneToOneField("Dish", on_delete=models.CASCADE)
 
     def __str__(self) -> str:
@@ -49,7 +46,8 @@ class Product(models.Model):
 
 
 class Dish(models.Model):
-    """This model represents a Dish. It has a dish name and a recipe is_favorite is added here to."""
+    """This model represents a Dish. It has a dish name and a recipe.
+    is_favorite is added here too."""
 
     name = models.CharField(max_length=100, unique=True)
     recipe = models.TextField()
@@ -63,10 +61,9 @@ class Dish(models.Model):
 
 
 class ProductDish(models.Model):
-    """This model represents the relation betweeen the product and a dish.
-
+    """This model represents the relation between the product and a dish.
     A product can have a certain amount of products in a dish.
-    Example: amount=4, unit=kg"""
+    Example: amount=4, unit=kg."""
 
     quantity = models.DecimalField(
         max_digits=10, decimal_places=2, null=True, blank=True
@@ -89,7 +86,7 @@ class ProductDish(models.Model):
 
 
 class Unit(models.Model):
-    """This model represents the unit. Is is used to measure a certain amount"""
+    """This model represents the unit. It is used to measure a certain amount."""
 
     name = models.CharField(max_length=20)
     abbreviation = models.CharField(max_length=15)
@@ -99,7 +96,7 @@ class Unit(models.Model):
 
 
 class ShoppingList(models.Model):
-    """This model represents the shoppinglist. Here all this is to link all the user to the list."""
+    """This model represents the shopping list. Here all this is to link all the user to the list."""
 
     date = models.DateTimeField(auto_now_add=True)
     # Foreign key
@@ -111,8 +108,8 @@ class ShoppingList(models.Model):
 
 
 class ProductShoppingList(models.Model):
-    """This model represents the relation of a product with/in a shoppinglist.
-    A user will be able to add products to his shoppinglist this way."""
+    """This model represents the relation of a product with/in a shopping list.
+    A user will be able to add products to his shopping list this way."""
 
     quantity = models.DecimalField(max_digits=10, decimal_places=2)
     # Foreign keys
@@ -127,6 +124,8 @@ class ProductShoppingList(models.Model):
 
 
 class UserMenu(models.Model):
+    """This model represents the relation between a user and a menu."""
+
     menu = models.ForeignKey("MenuList", on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -135,6 +134,8 @@ class UserMenu(models.Model):
 
 
 class DishMenu(models.Model):
+    """This model represents the relation between a menu and a dish."""
+
     menu = models.ForeignKey("MenuList", on_delete=models.CASCADE)
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
 
@@ -143,7 +144,7 @@ class DishMenu(models.Model):
 
 
 class MenuList(models.Model):
-    """This model represents a menu. The menu contains an amount of dishes linked to the user"""
+    """This model represents a menu. The menu contains an amount of dishes linked to the user."""
 
     name = models.CharField(max_length=50, unique=True)
 
@@ -152,7 +153,7 @@ class MenuList(models.Model):
 
 
 class BugReport(models.Model):
-    """This is so that users can report bug's only the developer will be able to see this."""
+    """This is so that users can report bugs. Only the developer will be able to see this."""
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, unique=True)
