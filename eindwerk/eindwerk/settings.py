62f7046afa9563b76f521e23a63afd4cb0b2d49c
Inias94 <128898984+Inias94@users.noreply.github.com>
2024-06-20 15:02:27 +0200

    Update settings.py
    allowed host tweak om te kunnen deployen

--- eindwerk/eindwerk/settings.py
+++ eindwerk/eindwerk/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
