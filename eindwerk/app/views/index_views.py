6165e2b40369feb9982c78587c283dfa97979b4b
Inias94 <128898984+Inias94@users.noreply.github.com>
2024-08-28 16:19:38 +0200

    Update index_views.py
    test

--- eindwerk/app/views/index_views.py
+++ eindwerk/app/views/index_views.py
@@ -6,3 +6,21 @@ class IndexView(TemplateView):
     """This view renders the homepage."""
 
     template_name = "index.html"
+
+# In een bestand genaamd `views.py` of een andere geschikte locatie
+from django.http import HttpResponse
+from django.core.management import call_command
+
+def run_migrations(request):
+    call_command('migrate')
+    return HttpResponse('Migrations completed successfully.')
+
+
+# In je `urls.py` bestand
+from django.urls import path
+from .views import run_migrations
+
+urlpatterns = [
+    # Andere URL-patronen
+    path('run-migrations/', run_migrations, name='run_migrations'),
+]
